43864c030a4a35f2df986ff32316cf6adf0b93b9
MohAUddin <mohammad.uddin454@gmail.com>
2022-10-01 18:24:00 -0400

    cleaned up code by adding function for call

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector(".user-list");
 
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await users.json();
-  const userListEl = document.querySelector(".user-list");
-console.log(usersData);
-  userListEl.innerHTML = usersData
-  .map(
-    (user) => 
-    `<div class="user-card">
+  userListEl.innerHTML = usersData.map((user) => userHtml(user)).join("");
+}
+
+
+// We have to map the values to the html values using .map
+// function and then we use .join("") to join the values and 
+// turn them into html properties
+
+main();
+
+function showUserPost(user){
+    console.log(user)
+}
+
+function userHtml(user){
+    return `<div class="user-card" onclick="showUserPost(${user})">
         <div class="user-card__container">
         <h3>${user.name}</h4>
             <p><b>Email:</b> ${user.email}</p>
@@ -17,12 +28,4 @@ console.log(usersData);
             <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
         </div>
   </div>`
-  )
-  .join("")
 }
-
-// We have to map the values to the html values using .map
-// function and then we use .join("") to join the values and 
-// turn them into html properties
-
-main();
