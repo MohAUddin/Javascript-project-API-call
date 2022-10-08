43e6a576b5b7844e87a625823d3f2832348af423
MohAUddin <mohammad.uddin454@gmail.com>
2022-10-07 22:53:31 -0400

    called the function

--- user.js
+++ user.js
@@ -1,5 +1,8 @@
-console.log(localStorage.get("id"));
-
 async function main(){
-    
+    const id = localStorage.getItem("id")
+    const post = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=:${id}`)
+    const postData = await post.json();
+    console.log(postData)
 }
+
+main();
